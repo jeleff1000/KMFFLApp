41509e61ac5b40684025cda34517bb4123eea2c2
jeleff1000 <joeyeleff@gmail.com>
2025-09-02 17:38:29 -0400

    stats

--- streamlit_ui/tabs/draft_data/draft_preferences.py
+++ streamlit_ui/tabs/draft_data/draft_preferences.py
@@ -232,7 +232,6 @@ def display_draft_preferences(draft_data, player_df):
     with tabs[1]:
         st.subheader("Cost Over Time")
         col1, col2 = st.columns(2)
-        # Clean manager list
         team_managers_clean = [m for m in draft_data['Team Manager'].dropna().unique() if m != 'League Total']
         team_managers = ['League Average'] + sorted(team_managers_clean)
         with col1:
@@ -243,15 +242,13 @@ def display_draft_preferences(draft_data, player_df):
             positions = ['All'] + positions_in_data
             selected_position_graph = st.selectbox("Position", positions, key="graph_position")
 
-        # All checkboxes in one expander
         with st.expander("Show/Hide Options", expanded=False):
             draft_col, keep_col = st.columns([1, 1])
             with draft_col:
                 show_drafted = st.checkbox("Drafted", value=True)
             with keep_col:
-                show_keepers = st.checkbox("Keepers", value=False)
+                show_keepers = st.checkbox("Keepers", value=True)  # Default to True
 
-            # Position checkboxes (only for 'All')
             if selected_position_graph == 'All':
                 cols = st.columns(len(positions_in_data))
                 include_positions = []
@@ -262,16 +259,11 @@ def display_draft_preferences(draft_data, player_df):
             else:
                 include_positions = None
 
-            # Dashed line checkboxes
-            if selected_manager_graph == 'League Average' and selected_position_graph == 'All':
-                default_dash = True
-                show_drafted_dash = st.checkbox("Show Drafted Dashed Line", value=default_dash)
-                show_keepers_dash = st.checkbox("Show Keepers Dashed Line", value=default_dash)
-                show_unused_dash = st.checkbox("Show Unused Dollars Dashed Line", value=default_dash)
-            else:
-                show_drafted_dash = False
-                show_keepers_dash = False
-                show_unused_dash = False
+            # Always show dashed line checkboxes for all managers
+            default_dash = True
+            show_drafted_dash = st.checkbox("Show Total Drafted Line", value=default_dash)
+            show_keepers_dash = st.checkbox("Show Total Keepers Line", value=default_dash)
+            show_unused_dash = st.checkbox("Show Total Unused Dollars Line", value=default_dash)
 
         graph_data = draft_data.copy()
         keeper_status = graph_data['Is Keeper Status']
@@ -366,7 +358,6 @@ def display_draft_preferences(draft_data, player_df):
                                 f"{col} Max: {max_player_text}"
                             )
                         else:
-                            # Filter for selected manager
                             rows = graph_data[
                                 (graph_data['Year'] == year) &
                                 (graph_data['Primary Position'] == col) &
@@ -409,7 +400,7 @@ def display_draft_preferences(draft_data, player_df):
                         else:
                             hover_texts.append('')
                     else:
-                        hover_texts = None  # No hover text for this case
+                        hover_texts = None
 
                 fig.add_trace(go.Scatter(
                     x=cost_pivot.index, y=y_vals,
@@ -418,7 +409,7 @@ def display_draft_preferences(draft_data, player_df):
                     hovertemplate='%{text}<extra></extra>' if hover_texts is not None else None
                 ))
 
-            # Dashed lines based on selected manager
+            # Dashed lines for all managers
             if selected_position_graph == 'All':
                 if selected_manager_graph == 'League Average':
                     num_managers = draft_data['Team Manager'].nunique()
@@ -434,13 +425,13 @@ def display_draft_preferences(draft_data, player_df):
                 if show_drafted and show_drafted_dash and not drafted_sum.empty:
                     fig.add_trace(go.Scatter(
                         x=drafted_sum.index, y=drafted_sum.values,
-                        mode='lines+markers', name='Drafted Dashed',
+                        mode='lines+markers', name='Drafted Total',
                         line=dict(dash='dash', color='blue')
                     ))
                 if show_keepers and show_keepers_dash and not keepers_sum.empty:
                     fig.add_trace(go.Scatter(
                         x=keepers_sum.index, y=keepers_sum.values,
-                        mode='lines+markers', name='Keepers Dashed',
+                        mode='lines+markers', name='Keepers Total',
                         line=dict(dash='dash', color='red')
                     ))
                 if show_unused_dash and not unused_dollars.empty:
@@ -461,13 +452,13 @@ def display_draft_preferences(draft_data, player_df):
                 if show_drafted and show_drafted_dash and not drafted_sum.empty:
                     fig.add_trace(go.Scatter(
                         x=drafted_sum.index, y=drafted_sum.values,
-                        mode='lines+markers', name='Drafted Dashed',
+                        mode='lines+markers', name='Drafted Total',
                         line=dict(dash='dash', color='blue')
                     ))
                 if show_keepers and show_keepers_dash and not keepers_sum.empty:
                     fig.add_trace(go.Scatter(
                         x=keepers_sum.index, y=keepers_sum.values,
-                        mode='lines+markers', name='Keepers Dashed',
+                        mode='lines+markers', name='Keepers Total',
                         line=dict(dash='dash', color='red')
                     ))
                 # No unused dollars line for positions other than 'All'
