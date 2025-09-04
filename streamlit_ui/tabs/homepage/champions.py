f131934baeb8ba6b7e63b902cb8fc54e81762656
jeleff1000 <joeyeleff@gmail.com>
2025-09-03 22:28:02 -0400

    stats

--- streamlit_ui/tabs/homepage/champions.py
+++ streamlit_ui/tabs/homepage/champions.py
@@ -2,6 +2,5 @@ import streamlit as st
 from tabs.hall_of_fame.hall_of_fame_homepage import HallOfFameViewer
 
 def display_champions(df_dict):
-    st.header("Champions")
     hall_of_fame_viewer = HallOfFameViewer(df_dict)
     hall_of_fame_viewer.display()
